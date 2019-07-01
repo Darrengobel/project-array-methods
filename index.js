b0f987c96bd62e1a994f80bb49ecc4765f3d449c
Darrengobel <Darren.Gobel@Gmail.com>
2019-07-01 12:00:29 +0700

    changes

---
+++ index.js
@@ -0,0 +1,18 @@
+let list = ['Stock01', 'Stock012', 'Stock02'];
+console.log(list)
+
+function press(){
+list.push(window.prompt("Would you kindly add more StockXX"))
+}
+
+console.log(list.shift())
+
+press();
+console.log(list)
+
+let listtwo = ['Dog', 'Cat', 'Turtle'];
+console.log(listtwo.sort())
+
+
+
+
